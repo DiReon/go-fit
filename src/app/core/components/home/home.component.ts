faaa9e45a574e48441e0e59178f99e659aafcca4
Dmitry Gorbachev <direon16@gmail.com>
2021-01-28 23:14:02 +0300

    Change view of HomePage
    - Add navigation as images

--- src/app/core/components/home/home.component.ts
+++ src/app/core/components/home/home.component.ts
@@ -30,36 +30,11 @@ export class HomeComponent implements OnInit, OnDestroy {
         if (!u.name) this.router.navigate(['user-profile']);
         this.date = formatDate(new Date(), 'yyyy-MM-dd', 'en');
         this.appUser = new AppUser(u);
-        this.load();
       };
     })
   }
 
   ngOnInit() {}
-
-  load() {
-    if (this.appUser.journal && this.appUser.journal!=null) {
-      console.log("Journal: ", this.appUser.journal);
-      console.log(Object.values(this.appUser.journal));
-      let record = Object.values(this.appUser.journal).filter(r => r.date == this.date)[0];
-      if (record) {
-        this.kkal = record.kkal;
-        this.steps = record.steps;
-        this.weight = record.weight;
-      } else this.kkal = this.steps = this.weight = null
-    }
-  }
-
-  save(value: MyRecord) {
-    let date = new Date().getTime();
-    value.dateRecorded = date;
-    console.log("Form value: ", value);
-    console.log(this.appUser);
-    
-    if (this.appUser.userId) this.userService.addToJournal(this.appUser.userId, value)
-    this.showConfirmationMsg = true;
-    this.router.navigate(['/'])
-  }
 
   ngOnDestroy() {
     this.subscription.unsubscribe()
