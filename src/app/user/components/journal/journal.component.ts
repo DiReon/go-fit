ad8701e162c84baf78c8f83f5ca1f0226c0be4f7
Dmitry Gorbachev <direon16@gmail.com>
2021-01-28 18:50:06 +0300

    Modified views
    - Replaced picture in NavBar with user name
    - Added bottomline below thimbnails in trainingListComponent
    - Removed photo from user-profile
    - Moved journal functionality to journal from HomePage

--- src/app/user/components/journal/journal.component.ts
+++ src/app/user/components/journal/journal.component.ts
@@ -1,4 +1,11 @@
+import { formatDate } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Subscription } from 'rxjs';
+import { AppUser } from 'src/app/shared/models/app-user';
+import { MyRecord } from 'src/app/shared/models/my-record';
+import { AuthService } from 'src/app/shared/services/auth.service';
+import { UserService } from 'src/app/shared/services/user.service';
 
 @Component({
   selector: 'app-journal',
@@ -6,10 +13,54 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./journal.component.css']
 })
 export class JournalComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+  appUser: AppUser;
+  weight: number;
+  showActions = true;
+  date: string;
+  showConfirmationMsg = false;
+  subscription: Subscription;
+  kkal: number;
+  steps: number;
+  constructor(
+    private authService: AuthService,
+    private userService: UserService,
+    private router: Router) { 
+    this.subscription = this.authService.appUser$.subscribe( u => {
+      if (u) {
+        if (!u.name) this.router.navigate(['user-profile']);
+        this.date = formatDate(new Date(), 'yyyy-MM-dd', 'en');
+        this.appUser = new AppUser(u);
+        this.load();
+      };
+    })
   }
 
-}
+  ngOnInit() {}
+
+  load() {
+    if (this.appUser.journal && this.appUser.journal!=null) {
+      console.log("Journal: ", this.appUser.journal);
+      console.log(Object.values(this.appUser.journal));
+      let record = Object.values(this.appUser.journal).filter(r => r.date == this.date)[0];
+      if (record) {
+        this.kkal = record.kkal;
+        this.steps = record.steps;
+        this.weight = record.weight;
+      } else this.kkal = this.steps = this.weight = null
+    }
+  }
+
+  save(value: MyRecord) {
+    let date = new Date().getTime();
+    value.dateRecorded = date;
+    console.log("Form value: ", value);
+    console.log(this.appUser);
+    
+    if (this.appUser.userId) this.userService.addToJournal(this.appUser.userId, value)
+    this.showConfirmationMsg = true;
+    this.router.navigate(['/'])
+  }
+
+  ngOnDestroy() {
+    this.subscription.unsubscribe()
+  }}
