ef16126e2494c68241a5e4a5f83a90fe853780c0
Dmitry Gorbachev <direon16@gmail.com>
2021-03-07 11:41:34 +0400

    Render calendar layout

--- src/app/user/components/calendar/calendar.component.ts
+++ src/app/user/components/calendar/calendar.component.ts
@@ -1,4 +1,5 @@
 import { formatDate } from '@angular/common';
+import { ThrowStmt } from '@angular/compiler';
 import { Component, OnInit } from '@angular/core';
 import { AppUser } from 'src/app/shared/models/app-user';
 import { MyRecord } from 'src/app/shared/models/my-record';
@@ -14,7 +15,10 @@ export class CalendarComponent implements OnInit {
   appUser: AppUser;
   days: MyRecord[] = [];
   date: Date;
-  month: number
+  month: number;
+  previousMonth: string;
+  nextMonth: string;
+  monthNames = ['Январь', 'Февраль', 'Март', 'Апрель', 'Май', 'Июнь', 'Июль', 'Август', 'Сентябрь', 'Октябрь', 'Ноябрь', 'Декабрь'];
   constructor(
     private authService: AuthService,
     private userService: UserService,
@@ -23,48 +27,21 @@ export class CalendarComponent implements OnInit {
     this.authService.appUser$.subscribe(u => {
       this.appUser = new AppUser(u);
       this.date = new Date();
-      this.month = this.date.getMonth()-1;
-      console.log(this.month);
-      
-      switch (this.month+1) {
-        case 1 || 3 || 5 || 7 || 8 || 10 || 12:
-        this.setMonth(31);  
-          break;
-        case 4 || 6 || 9 || 11:
-          this.setMonth(30);
-          break;
-        case 2:
-          this.setMonth((this.date.getFullYear() % 4 === 0) ? 29 : 28);
-          break;
-      }
-      console.table(this.appUser);
-      
-      console.log('this.appUser.journal', this.appUser.journal);
-      
-      let journal = (this.appUser.journal) ? Object.values(this.appUser.journal) : [];
-      console.log('Journal', journal);
-      
-      this.days.forEach(d => {
-        let record = journal.filter(r => r.date == d.date)[0];
-        console.log('Record: ', record);
-        
-        if (record) {
-          d.kkal = record.kkal;
-          d.steps = record.steps;
-          d.weight = record.weight;
-          d.trainingTitles = record.trainingTitles;
-        } else d.kkal = d.steps = d.weight = d.trainingTitles= null
-      })
-
+      //this.month = this.date.getMonth();
+      this.month = 1
+      this.loadMonth(0);
     })
   }
 
   ngOnInit(): void {
   }
 
-  setMonth(value) {
-    for (let index = 1; index < value+1; index++) {
+  setMonth(value: number) {
+    console.log("number of days in the month: ", value);
+    
+    for (let index = 1; index < value + 1; index++) {
       let dateForm = formatDate(new Date(this.date.getFullYear(), this.month, index), 'yyyy-MM-dd', 'en');
+      console.log(dateForm);
       
       let emptyDay: MyRecord = {
         date: dateForm,
@@ -80,4 +57,37 @@ export class CalendarComponent implements OnInit {
     }
   }
 
+  clearMonth() {
+    this.days = [];
+  }
+
+  loadMonth(value: number) {
+    this.clearMonth();
+    this.month += value;
+    
+    this.previousMonth = (this.month < 1) ? this.monthNames[11] : this.monthNames[this.month - 1];
+    this.nextMonth = (this.month > 10) ? this.monthNames[0] : this.monthNames[this.month + 1];
+    console.log("this.month+1: ", this.month+1);
+    
+    if ([1, 3, 5, 7, 8, 10, 12].indexOf(this.month+1) != -1)  { 
+      console.log("this.month set to: ", (this.month + 1));
+      this.setMonth(31);
+    } 
+    else if ([4, 6, 9, 11].indexOf(this.month+1) != -1) { this.setMonth(30) }
+    else if  (this.month + 1 == 2) { this.setMonth((this.date.getFullYear() % 4 === 0) ? 29 : 28); };
+    
+    let journal = (this.appUser.journal) ? Object.values(this.appUser.journal) : [];
+    console.log('Journal', journal);
+    
+    this.days.forEach(d => {
+      let record = journal.filter(r => r.date == d.date)[0];
+      
+      if (record) {
+        d.kkal = record.kkal;
+        d.steps = record.steps;
+        d.weight = record.weight;
+        d.trainingTitles = record.trainingTitles;
+      } else d.kkal = d.steps = d.weight = d.trainingTitles= null;
+    })
+  }
 }
