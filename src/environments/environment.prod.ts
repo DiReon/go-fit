8d885f34f88d9d6daa534f2b6759f49f997a50d7
Dmitry Gorbachev <direon16@gmail.com>
2021-04-12 14:16:52 +0400

    Deploy app to new firebase hosting and database
    - remove all firebase files
    - create new firebase web app
    - initialize directory and select app
    - change environment.ts settings
    - import database from regina-go main app
    - deploy application to firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,13 +1,13 @@
 export const environment = {
   production: true,
   firebase: {
-    apiKey: "AIzaSyDspXu8Enb-nhOIoME8r2WCpbxk4AX5kRo",
-    authDomain: "regina-go.firebaseapp.com",
-    databaseURL: "https://regina-go.firebaseio.com",
-    projectId: "regina-go",
-    storageBucket: "regina-go.appspot.com",
-    messagingSenderId: "746116244865",
-    appId: "1:746116244865:web:5c78175bd2c6e30f8b3480",
-    measurementId: "G-NM7EKRPBWN"
+    apiKey: "AIzaSyBgOdcQXlUBU23oNpSh_MEZcVczxqqv35M",
+    authDomain: "reginarus-go.firebaseapp.com",
+    databaseURL: "https://reginarus-go-default-rtdb.firebaseio.com/",
+    projectId: "reginarus-go",
+    storageBucket: "reginarus-go.appspot.com",
+    messagingSenderId: "987135104799",
+    appId: "1:987135104799:web:416dac2dd4c91986b7c22a",
+    measurementId: "G-0D4MB8WTPL"
   }
 };
