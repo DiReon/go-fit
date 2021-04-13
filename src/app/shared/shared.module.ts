15fa05ea6d6e561e06ac79bff1ebdc30257c6fe9
Dmitry Gorbachev <direon16@gmail.com>
2021-04-13 17:06:23 +0400

    Update UserProfileComponent
    - Update HTML with Angular Material
    - Remove Bootstrap classes
    - Deploy and test on mobile

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,7 +3,10 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
+import { MatButtonToggleModule } from '@angular/material/button-toggle';
 import { MatCardModule } from '@angular/material/card';
+import { MatNativeDateModule } from '@angular/material/core';
+import { MatDatepickerModule } from '@angular/material/datepicker';
 import { MatExpansionModule } from '@angular/material/expansion';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatGridListModule } from '@angular/material/grid-list';
@@ -11,6 +14,7 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
 import { MatListModule } from '@angular/material/list';
 import { MatMenuModule } from '@angular/material/menu';
+import { MatSelectModule } from '@angular/material/select';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatSliderModule } from '@angular/material/slider';
 import { MatSnackBarModule } from '@angular/material/snack-bar';
@@ -20,7 +24,7 @@ import { YouTubePlayerModule } from '@angular/youtube-player';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { NgxDatatableModule } from '@swimlane/ngx-datatable';
-import {MatDatepickerModule} from '@angular/material/datepicker';
+
 import { AppRoutingModule } from '../app-routing.module';
 import { MealCardComponent } from './components/meal-card/meal-card.component';
 import { PictureTilesComponent } from './components/picture-tiles/picture-tiles.component';
@@ -30,8 +34,7 @@ import { AuthService } from './services/auth.service';
 import { MealService } from './services/meal.service';
 import { SharedService } from './services/shared.service';
 import { UserService } from './services/user.service';
-import { MatNativeDateModule } from '@angular/material/core';
-import {MatButtonToggleModule} from '@angular/material/button-toggle';
+
 @NgModule({
   declarations: [
     MealCardComponent,
@@ -63,6 +66,7 @@ import {MatButtonToggleModule} from '@angular/material/button-toggle';
     MatSnackBarModule,
     MatDatepickerModule,
     MatButtonToggleModule,
+    MatSelectModule,
   ],
   exports: [
     AppRoutingModule,
@@ -92,6 +96,7 @@ import {MatButtonToggleModule} from '@angular/material/button-toggle';
     MatDatepickerModule,
     MatNativeDateModule,
     MatButtonToggleModule,
+    MatSelectModule,
   ],
   providers: [
     AuthService,
